ad98b8ff8a241a353f3f90f1f8f23b4c7825002b
Arpan Purkait <arpanpurkait743354@gmail.com>
2025-10-06 16:28:59 +0530

    now it's correct

--- helloWorld/django_project/pages/tests.py
+++ helloWorld/django_project/pages/tests.py
@@ -3,7 +3,7 @@ from django.test import SimpleTestCase
 # Create your tests here.
 class HomeTest(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
-        response = self.client.get("/about")
+        response = self.client.get("/")
         self.assertEqual(response.status_code,200)
 
 
