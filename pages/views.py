4a1c05493ee5495234f5bbb7709f6507235de530
Arpan Purkait <arpanpurkait743354@gmail.com>
2025-10-07 04:41:56 +0530

    final comit

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+def home_page_view(request): # new
+    context = {
+        "inventory_list": ["laptop", "desktop", "smartphone", "tablet"],
+        "greeting": "THank You foR visitING. !",
+
+    }
+    return render(request, "home.html",context)
+
+class AboutPageView(TemplateView): # new
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Main Street"
+        context["phone_number"] = "829-369-0911"
+        return context
