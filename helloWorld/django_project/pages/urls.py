489fe4b0ac20e0b1018c46ded5a35332d2ba31cf
Arpan Purkait <arpanpurkait743354@gmail.com>
2025-10-06 16:26:02 +0530

    hi

--- helloWorld/django_project/pages/urls.py
+++ helloWorld/django_project/pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
 from .views import home_page_view
+from .views import about_page_view
 
 urlpatterns = [
     
     path("", home_page_view, name="home" ),
+    path("about/", about_page_view, name="about")
 ]
 
 
--- helloWorld/django_project/pages/views.py
+++ helloWorld/django_project/pages/views.py
@@ -1,6 +1,16 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 def home_page_view(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Home Page")
+
+
+def about_page_view(request):
+    context = {
+        "name": "Arpan",
+        "age": 21
+    }
+    return render(request,"pages/about.html",context)
+
 
 # hey I am testing the git ....
 # is this working...?
